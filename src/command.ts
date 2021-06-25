996617683cfa2adfb5e2c77b90af7a7e3dff1a0d
Thibault Duplessis <t@lichess.org>
2021-06-25 19:03:56 +0200

    basic commands

--- src/command.ts
+++ src/command.ts
@@ -8,11 +8,16 @@ export interface Fortune {
   verb: 'fortune';
 }
 
-type Command = Hi | Fortune;
+export interface Help {
+  verb: 'help';
+}
+
+type Command = Hi | Fortune | Help;
 
 export const parseCommand = (cmd: string, _orig: ZulipOrig): Command => {
   const verb = cmd.split(' ')[0].toLowerCase();
   if (verb == 'fortune') return { verb };
   if (verb == 'hi' || verb == 'hello') return { verb: 'hi' };
+  if (verb == 'help' || verb == 'halp' || verb == 'h') return { verb: 'help' };
   throw "I don't get it.";
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as zulipInit from 'zulip-js';
 import { Zulip, ZulipMsg, messageLoop, reply, send, react, ZulipDestPrivate, botName } from './zulip';
+import { parseCommand } from './command';
+import fetch from 'node-fetch';
 
 (async () => {
   const z: Zulip = await zulipInit.default({ zuliprc: 'zuliprc' });
@@ -9,14 +11,11 @@ import { Zulip, ZulipMsg, messageLoop, reply, send, react, ZulipDestPrivate, bot
     try {
       const command = parseCommand(msg.command, msg);
       switch (command.verb) {
-        case 'list':
-          await listReminders(msg);
+        case 'hi':
+          reply(z, msg, ':wave: Why hello there!');
           break;
-        case 'remind':
-          await addReminder(msg, command);
-          break;
-        case 'delete':
-          await deleteReminder(msg, command.id);
+        case 'fortune':
+          await fortune(msg);
           break;
         case 'help':
           await help(msg);
@@ -28,66 +27,17 @@ import { Zulip, ZulipMsg, messageLoop, reply, send, react, ZulipDestPrivate, bot
     }
   };
 
-  const addReminder = async (msg: ZulipMsg, remind: Remind) => {
-    remind.id = await store.add(remind);
-    console.log(printRemind(remind));
-    await reply(z, msg, `:check_mark: ${printRemind(remind)}`);
-  };
-
-  const listReminders = async (msg: ZulipMsg) => {
-    const all = await store.list();
-
-    if (msg.type == 'stream') {
-      await reply(z, msg, 'Public reminders in this stream:');
-      const publicReminders = all.filter(r => r.dest.type == 'stream' && r.dest.to == msg.stream_id);
-      if (publicReminders.length) publicReminders.forEach(async r => await reply(z, msg, printRemind(r)));
-      else await reply(z, msg, 'None');
-    }
-
-    const privateReminders = all.filter(r => r.dest.type == 'private' && r.from == msg.sender_id);
-    if (privateReminders.length || msg.type == 'private') {
-      const pm: ZulipDestPrivate = { type: 'private', to: [msg.sender_id] };
-      await send(z, pm, 'Your private reminders:');
-      if (privateReminders.length) privateReminders.forEach(async r => await send(z, pm, printRemind(r)));
-      else await send(z, pm, 'None');
-    }
-  };
-
-  const deleteReminder = async (msg: ZulipMsg, id: RemindId) => {
-    const done = await store.delete(id, msg.sender_id);
-    await react(z, msg, done ? 'check_mark' : 'cross_mark');
+  const fortune = async (msg: ZulipMsg) => {
+    const res = await fetch('https://api.ef.gy/fortune');
+    const text = await res.text();
+    await reply(z, msg, text);
   };
 
   const help = async (msg: ZulipMsg) => {
     const name = await botName(z);
     const mention = `@${name}`;
-    await reply(
-      z,
-      msg,
-      [
-        'Use `' + mention + '` to set a reminder for yourself, or for a stream.',
-        'Some examples include:',
-        '- `' + mention + ' me on June 1st to wish Linda happy birthday`',
-        '- `' + mention + ' me to stop procrastinating tomorrow`',
-        '- `' + mention + ' here in 3 hours to update the project status`',
-        '- `' + mention + ' stream to party hard on 2021-09-27 at 10pm`',
-        '',
-        'Use `' + mention + ' list` to see the list of all your reminders.',
-        '',
-        'Use `' + mention + ' delete id` to delete a reminder by its ID',
-      ].join('\n')
-    );
+    await reply(z, msg, ['- `' + mention + ' fortune` get a Linux kernel fortune message'].join('\n'));
   };
-
-  const remindNow = async (add: Remind) => {
-    console.log('Reminding now', add);
-    await send(z, add.dest, `Reminder: ${add.what}`);
-  };
-
-  setInterval(async () => {
-    const add = await store.poll();
-    if (add) await remindNow(add);
-  }, 1000);
 
   await messageLoop(z, messageHandler);
 })();
